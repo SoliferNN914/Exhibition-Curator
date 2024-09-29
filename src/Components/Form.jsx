0f8e1759f85593b53fd29cd465b3888787c58023
SoliferNN914 <oskarson16@gmail.com>
2024-09-29 12:45:26 +0300

    removed search input clearing on handlesubmit

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -105,7 +105,7 @@ export default function Form({ searchRequest, setSearchRequest, searchedItem, se
     event.preventDefault();
     setSearchedItem(searchRequest);
     setFilters(prevFilters => ({ ...prevFilters, startYear, endYear }));
-    setSearchRequest('');
+    //setSearchRequest('');
   };
 
   return (
